b04af527f37c9381e07c692340b1af64813411a4
ignacionar <ignacionarvajadev@gmail.com>
2022-10-13 11:20:52 -0700

    Second Commit

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
+import { connectDB } from './db/db';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
