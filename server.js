409a64d1d7f07e54dea119a2e4fb8fd6b5ad17c0
ignacionar <ignacionarvajadev@gmail.com>
2022-10-13 11:39:41 -0700

    xd

--- server.js
+++ server.js
@@ -20,10 +20,10 @@ server.use(cors());
 server.use(express.json());
 
 if (process.env.NODE_ENV === "production") {
-  server.use(express.static(path.join(__dirname, '/client/src')));
+  server.use(express.static(path.join(__dirname, '/client/build')));
     
   server.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + 'client', 'public', 'index.html'));
+    res.sendFile(path.join(__dirname + 'client', 'build', 'index.html'));
   })
 } else {
   server.get('/', (req, res) => {
